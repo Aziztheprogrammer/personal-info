c0f515a3af38e75efe13894e29bda78775fb3e12
Aziz Berhouma <aziz.theprogrammer@gmail.com>
2025-07-10 11:45:20 +0100

    Add files via upload

--- constants.js
+++ constants.js
@@ -20,7 +20,7 @@ const testimonials = [
     {
         clientName: "Jawaher Alnaser",
         clientTitle: "Makeup Artist",
-        clientImage: "./assets/1.jpg",
+        clientImage: "assets/1.jpg",
         content: "A Beautiful Work Aziz, Thank You !!",
         rating: 5,
     },
@@ -28,42 +28,42 @@ const testimonials = [
 
 const works = [
     {
-        workImage: "./assets/2.png",
+        workImage: "assets/2.png",
         workTitle: "Jawaher Alnaser - Makeup Artist Portfolio",
         workDescription : "This Project Is A Portfolio Website Built With HTML, CSS, and JS For Our Client Jawaher Alnaser.",
         githubLink: "https://aziztheprogrammer.github.io/Makeup-Artist-Portfolio-Project/",
         behanceLink: "https://www.behance.net/gallery/194590411/Jawaher-Alnaser-Makeup-Artist-Portfolio-Design"
     },
     {
-        workImage: "./assets/3.png",
+        workImage: "assets/3.png",
         workTitle: "PettyShop - Website Design Of Pets Shop",
         workDescription : "This Project Is An Online Shop Built With HTML, CSS, and JS For Our Client Jawaher Alnaser.",
         githubLink: "https://aziztheprogrammer.github.io/PettyShop-Project/",
         behanceLink: ""
     },
     {
-        workImage: "./assets/4.png",
+        workImage: "assets/4.png",
         workTitle: "Leon - Landing Page",
         workDescription : "This Template Is Not Made By Me, I'm Just Using It To Practice On CSS And HTML.",
         githubLink: "https://aziztheprogrammer.github.io/Leon-Template/",
         behanceLink: ""
     },
     {
-        workImage: "./assets/5.png",
+        workImage: "assets/5.png",
         workTitle: "Fylo - Landing Page",
         workDescription : "Frontend Mentor Challenge 18 : Fylo Landing Page With Two Column Layout Master",
         githubLink: "https://aziztheprogrammer.github.io/FrontEnd-Mentor-18/",
         behanceLink: ""
     },
     {
-        workImage: "./assets/6.png",
+        workImage: "assets/6.png",
         workTitle: "isDone - Manage Your Tasks",
         workDescription : "isDone Is A Platform Developed By Aziz Berhouma To Help Others Planning Their Life And Setting Their Daily Tasks And Goals, This Project Is Developed With HTML, CSS, JS, TailwindCSS :) !",
         githubLink: "https://aziztheprogrammer.github.io/isDone-Project/",
         behanceLink: ""
     },
     {
-        workImage: "./assets/7.png",
+        workImage: "assets/7.png",
         workTitle: "EngineerPort - Software Engineer Portfolio",
         workDescription : "This Project Is A Website Built With HTML, CSS, and JS As A Software Engineer Portfolio Design.",
         githubLink: "https://aziztheprogrammer.github.io/SE-Portfolio-Project/",
